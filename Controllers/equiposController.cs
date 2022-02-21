c66da61e720b5fdcc1a3b13b332b8369531ee7e0
MarcoEsquivel1 <marco.esquivel@catolica.edu.sv>
2022-02-21 09:54:41 -0600

    Primer update

---
+++ Controllers/equiposController.cs
@@ -0,0 +1,36 @@
+﻿using equiposWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace equiposWebApi.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly prestamosContext _contexto;
+
+        public equiposController(prestamosContext miContexto) 
+        {
+            this._contexto = miContexto;
+        }
+
+        [HttpGet]
+        [Route("api/equipos")]
+        public IActionResult Get()
+        {
+            IEnumerable<equipos> equiposList = from e in _contexto.equipos 
+                                                select e;
+            if (equiposList.Count() > 0)
+            {
+                return Ok(equiposList);
+            }
+            return NotFound();
+        }
+    }
+}
