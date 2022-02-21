39a5a7c81b2d812e9f4994a5c29ffcedd0bd96b8
MarcoEsquivel1 <marco.esquivel@catolica.edu.sv>
2022-02-21 12:05:59 -0600

    CRUD completado

--- Controllers/equiposController.cs
+++ Controllers/equiposController.cs
@@ -24,13 +24,91 @@ namespace equiposWebApi.Controllers
         [Route("api/equipos")]
         public IActionResult Get()
         {
-            IEnumerable<equipos> equiposList = from e in _contexto.equipos 
-                                                select e;
-            if (equiposList.Count() > 0)
+            try
             {
-                return Ok(equiposList);
+                IEnumerable<equipos> equiposList = from e in _contexto.equipos
+                                                   select e;
+                if (equiposList.Count() > 0)
+                {
+                    return Ok(equiposList);
+                }
+                return NotFound();
             }
-            return NotFound();
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("api/equipos/{idEquipo}")]
+        public IActionResult Get(int idEquipo)
+        {
+            try
+            {
+                equipos equipo = (from e in _contexto.equipos
+                                  where e.id_equipos == idEquipo
+                                  select e
+                                ).FirstOrDefault();
+
+                if (equipo != null)
+                {
+                    return Ok(equipo);
+                }
+                return NotFound();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("api/equipos")]
+        public IActionResult guardarEquipo([FromBody] equipos equipoNuevo)
+        {
+            try
+            {
+                _contexto.equipos.Add(equipoNuevo);
+                _contexto.SaveChanges();
+                return Ok(equipoNuevo);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [Route("api/equipos")]
+        public IActionResult updateEquipo([FromBody] equipos equipoAModificar)
+        {
+            try
+            {
+                equipos equipoExiste = (from e in _contexto.equipos
+                                        where e.id_equipos == equipoAModificar.id_equipos
+                                        select e).FirstOrDefault();
+
+                if (equipoExiste is null)
+                {
+                    return NotFound();
+                }
+
+                equipoExiste.nombre = equipoAModificar.nombre;
+                equipoExiste.descripcion = equipoAModificar.descripcion;
+
+                _contexto.Entry(equipoExiste).State = EntityState.Modified;
+                _contexto.SaveChanges();
+
+                return Ok(equipoExiste);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
     }
 }
